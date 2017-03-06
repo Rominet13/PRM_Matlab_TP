c42ea1a602225e1b8c9df76dddb36fbee9fcc86e
Rominet13 <volphoenix@outlook.fr>
2017-03-06 09:18:46 +0100

    Delete addToOpenSet.m

--- addToOpenSet.m
+++
@@ -1,9 +0,0 @@
-function addToOpenSet( node , predecessor , evaluation ) ;
-global nodesInOpenSet numberOfNodes openSet
-    % Insert node in openSet. Here openSet is not ordered
-    if nodesInOpenSet == numberOfNodes ,
-        error('Impossible to add a node in openSet: full.');
-    end
-    nodesInOpenSet = nodesInOpenSet + 1 ;
-    openSet(nodesInOpenSet,:) = [ node predecessor evaluation ] ;
-end
